e3196535aaf337a4ca9d2c1e51a938ed20149706
topfiga <topfiga@gmail.com>
2026-04-24 15:48:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 app = FastAPI()
+
+# inicializa firebase (usa variável de ambiente depois)
+cred = credentials.Certificate("firebase-key.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 @app.get("/")
 def home():
     return {"status": "ok"}
 
-@app.get("/preco/{produto}")
-def preco(produto: str):
-    precos = {
-        "soja": 150,
-        "milho": 80
-    }
+@app.get("/salvar/{produto}/{preco}")
+def salvar(produto: str, preco: float):
+    db.collection("precos").document(produto).set({
+        "valor": preco
+    })
 
-    return {
-        "produto": produto,
-        "preco": precos.get(produto, "não encontrado")
-    }
+    return {"msg": "salvo"}
+
+@app.get("/preco/{produto}")
+def buscar(produto: str):
+    doc = db.collection("precos").document(produto).get()
+
+    if doc.exists:
+        return doc.to_dict()
+    else:
+        return {"erro": "não encontrado"}
